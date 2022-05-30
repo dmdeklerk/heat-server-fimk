d194770be226bfbf5143d865be4e795d16d47d2e
Dennis de Klerk <dmdeklerk@gmail.com>
2022-05-30 16:10:20 +0200

    sellerPublicKey

--- dist/modules/custom_fimk_dgs_good.js
+++ dist/modules/custom_fimk_dgs_good.js
@@ -2,6 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.customFimkDgsGood = void 0;
 const heat_server_common_1 = require("heat-server-common");
+const lodash_1 = require("lodash");
+const publickey_lookup_1 = require("./publickey_lookup");
 async function customFimkDgsGood(context, param) {
     try {
         const { req, protocol, host, logger } = context;
@@ -9,6 +11,11 @@ async function customFimkDgsGood(context, param) {
         const url = `${protocol}://${host}?requestType=getDGSGood&goods=${goods}&includeCounts=${includeCounts}`;
         const json = await req.get(url);
         const data = (0, heat_server_common_1.tryParse)(json, logger);
+        const { seller } = data;
+        if ((0, lodash_1.isString)(seller)) {
+            const publicKey = await getPublicKey(context, param.blockchain, seller);
+            data.sellerPublicKey = publicKey;
+        }
         return {
             value: data,
         };
@@ -20,4 +27,26 @@ async function customFimkDgsGood(context, param) {
     }
 }
 exports.customFimkDgsGood = customFimkDgsGood;
+const publicKeyCache = new Map();
+async function getPublicKey(context, blockchain, addrXpub) {
+    var _a, _b, _c;
+    const { logger } = context;
+    if (publicKeyCache.has(addrXpub)) {
+        return publicKeyCache.get(addrXpub);
+    }
+    const response = await (0, publickey_lookup_1.publicKeyLookup)(context, { blockchain, addrXpub });
+    if (response.error) {
+        logger.error(`public key lookup failed: ${response.error}`);
+        return '';
+    }
+    else if (!(0, lodash_1.isString)((_a = response.value) === null || _a === void 0 ? void 0 : _a.publicKey)) {
+        logger.error(`public key lookup returns null`);
+        return '';
+    }
+    const publicKey = (_c = (_b = response.value) === null || _b === void 0 ? void 0 : _b.publicKey) !== null && _c !== void 0 ? _c : '';
+    if (!(0, lodash_1.isEmpty)(publicKey)) {
+        publicKeyCache.set(addrXpub, publicKey);
+    }
+    return publicKey;
+}
 //# sourceMappingURL=custom_fimk_dgs_good.js.map
--- src/modules/custom_fimk_dgs_good.ts
+++ src/modules/custom_fimk_dgs_good.ts
@@ -1,10 +1,13 @@
 import {
+  Blockchains,
   CallContext,
   CustomFimkDgsGoodParam,
   CustomFimkDgsGoodResult,
   ModuleResponse,
   tryParse,
 } from "heat-server-common";
+import { isEmpty, isString } from "lodash";
+import { publicKeyLookup } from './publickey_lookup'
 
 export async function customFimkDgsGood(
   context: CallContext,
@@ -15,7 +18,12 @@ export async function customFimkDgsGood(
     const { goods, includeCounts } = param;
     const url = `${protocol}://${host}?requestType=getDGSGood&goods=${goods}&includeCounts=${includeCounts}`;
     const json = await req.get(url);
-    const data = tryParse(json, logger);
+    const data: CustomFimkDgsGoodResult = tryParse(json, logger);
+    const { seller } = data;
+    if (isString(seller)) {
+      const publicKey = await getPublicKey(context, param.blockchain, seller);
+      data.sellerPublicKey = publicKey;
+    }
     return {
       value: data,
     };
@@ -24,4 +32,27 @@ export async function customFimkDgsGood(
       error: e.message,
     };
   }
+}
+
+const publicKeyCache = new Map<string, string>();
+async function getPublicKey(context: CallContext, blockchain: Blockchains, addrXpub: string,): Promise<string> {
+  const { logger } = context
+  if (publicKeyCache.has(addrXpub)) {
+    return publicKeyCache.get(addrXpub);
+  }
+
+  const response = await publicKeyLookup(context, { blockchain, addrXpub });
+  if (response.error) {
+    logger.error(`public key lookup failed: ${response.error}`);
+    return ''
+  }
+  else if (!isString(response.value?.publicKey)) {
+    logger.error(`public key lookup returns null`);
+    return ''
+  }
+  const publicKey = response.value?.publicKey ?? '';
+  if (!isEmpty(publicKey)) {
+    publicKeyCache.set(addrXpub, publicKey);
+  }
+  return publicKey;
 }
